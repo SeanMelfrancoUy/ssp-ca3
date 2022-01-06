a670ab4183f77a8ff815361d91bf41168b29d63a
SeanMelfrancoUy <seanmelfrancouy@outlook.com>
2022-01-06 16:45:28 +0000

    scripts.js

--- views/js/scripts.js
+++ views/js/scripts.js
@@ -5,7 +5,7 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-//The following 7 lines of code (or lines 10 to 16) has been sourced from https://github.com/mikhail-cct/ssp-practical //
+//The following 7 lines of code (or lines 10 to 16) has been sourced from https://github.com/mikhail-cct/ssp-practical 
 
 window.addEventListener("load", function() {
     // document.forms[1].txtBillAmt.value = calculateBill('menuTable');
